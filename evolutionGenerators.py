Corrected forward variance in the lognormal model

evolutionGenerators.py
@@ -35,7 +35,7 @@ class EvolutionGeneratorLognormal(EvolutionGeneratorMonteCarloBase):
         self.sampleValues = {}
 
         for i,it in enumerate(dates):
-            dW = torch.sqrt(self.variances[0]) * z[:,i] - self.variances[0]/2.
+            dW = torch.sqrt(self.variances[i]) * z[:,i] - self.variances[i]/2.
             logsamples = logsamples + dW
 
             self.sampleValues[it] = {}

models.py
@@ -35,8 +35,15 @@ class LognormalModel(BaseModel):
         # Derive the timeline from the product
         dates_underlyings = productData.getDatesUnderlyings()
         dates = np.array(list(dates_underlyings.keys()))
-        times = torch.from_numpy(np.asarray((dates[0] - self.modelDate).days/365))
-        variances = volatility * volatility * times
+
+        var = []
+        lastDate = self.modelDate
+        for it in dates_underlyings.keys():
+            dt = torch.from_numpy(np.asarray((it - lastDate).days / 365))
+            var.append( volatility * volatility * dt )
+            lastDate = it
+
+        variances = torch.tensor(var)
 
 
         return evolutionGenerators.EvolutionGeneratorLognormal(simulationData, dates_underlyings, fwd, variances)

products.py
@@ -56,22 +56,15 @@ class AsianOptionProduct(BaseProduct):
         for it in self.observationDates:
             self.dates_underylings[it] = self.index
 
-    def getStrike(self):
-        return self.strike
-
     def getPayoff(self, evolutionGenerator : evolutionGenerators.EvolutionGeneratorMonteCarloBase):
         strike = self.data['strike']
         index = self.data['index']
 
-        indexValues = evolutionGenerator.getSampleValues(self.observationDates[0], index)
-        for i in range(self.numberOfObservationDates - 1):
-            indexValues = indexValues + evolutionGenerator.getSampleValues(self.observationDates[i], index)
+        avg = torch.tensor(0.)
+        for it in self.observationDates :
+            avg = avg + evolutionGenerator.getSampleValues(it, index)
 
-        return torch.max(indexValues / self.numberOfObservationDates - strike, torch.tensor(0.))
-
-    def productData(self):
-        return productData.ProductDataBase(self.dates_underylings)
-
+        return torch.max(avg / torch.tensor(self.numberOfObservationDates) - strike, torch.tensor(0.))
 
 if __name__ == '__main__':
     expiry = datetime.date(year=2021, month=12, day=30)