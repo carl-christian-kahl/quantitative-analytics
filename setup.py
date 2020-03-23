868cc443d415eaf15e6d8b0c5d26cca5b9fcf921
carl-christan-kahl <carl.christian.kahl@googlemail.com>
2020-03-23 10:26:14 +0000

    Changed setup file
    Checked in by: CK

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='quantitative_analytics', version='1.0', packages=find_packages())
+setup(
+    name='quantitative_analytics',
+    version='0.1.0',
+    packages=find_packages(),
+    description = 'Quantitative analytics package',
+    url = 'git@github.com:carl-christian-kahl/quantitative-analytics.git',
+    author = 'Christian Kahl',
+    author_email = 'carl.christian.kahl@googlemail.com',
+    license = 'unlicense',
+    zip_safe=False
+)
